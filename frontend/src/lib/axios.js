5689855a3088da1e01539b6f49f3c3ae517d88ba
LoayAhmed304 <loayahmed304@gmail.com>
2025-07-12 20:50:30 +0000

    chore: change project structure for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_BACKEND_URL + '/api' || 'http://localhost:5000/api',
+    import.meta.env.VITE_API_URL + '/api' || 'http://localhost:5000/api',
   withCredentials: true,
 });
